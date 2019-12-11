01a38101408c09910f264557bf379678f4cd271b
chq <17875511931@163.com>
2019-12-11 09:59:41 +0800

    '登录界面'

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -2,34 +2,18 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>{{ isset($pageTitle) ? $pageTitle . ' | ' : '' }}{{ setting('app-name') }}</title>
+    <title>{{ isset($pageTitle) ? $pageTitle . ' | ' : '' }}</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta http-equiv="Access-Control-Allow-Origin" content="*">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="format-detection" content="telephone=no">
-    <link rel="stylesheet" href="../lib/layui-v2.5.4/css/layui.css
-" media="all">
-    <style>
-        body {background-image:url("../images/bg.jpg");height:100%;width:100%;}
-        #container{height:100%;width:100%;}
-        input:-webkit-autofill {-webkit-box-shadow:inset 0 0 0 1000px #fff;background-color:transparent;}
-        .admin-login-background {width:300px;height:300px;position:absolute;left:50%;top:40%;margin-left:-150px;margin-top:-100px;}
-        .admin-header {text-align:center;margin-bottom:20px;color:#ffffff;font-weight:bold;font-size:40px}
-        .admin-input {border-top-style:none;border-right-style:solid;border-bottom-style:solid;border-left-style:solid;height:50px;width:300px;padding-bottom:0px;}
-        .admin-input::-webkit-input-placeholder {color:#a78369}
-        .layui-icon-username {color:#a78369 !important;}
-        .layui-icon-username:hover {color:#9dadce !important;}
-        .layui-icon-password {color:#a78369 !important;}
-        .layui-icon-password:hover {color:#9dadce !important;}
-        .admin-input-username {border-top-style:solid;border-radius:10px 10px 0 0;}
-        .admin-input-verify {border-radius:0 0 10px 10px;}
-        .admin-button {margin-top:20px;font-weight:bold;font-size:18px;width:300px;height:50px;border-radius:5px;background-color:#a78369;border:1px solid #d8b29f}
-        .admin-icon {margin-left:260px;margin-top:10px;font-size:30px;}
-        i {position:absolute;}
-        .admin-captcha {position:absolute;margin-left:205px;margin-top:-40px;}
-    </style>
+    <link rel="stylesheet" href="{{ URL::asset('layuimini/lib/layui-v2.5.4/css/layui.css') }}" media="all">
+
+    <script src="{{ URL::asset('layuimini/lib/layui-v2.5.4/layui.js') }}" charset="utf-8"></script>
+
+    @yield('style')
 </head>
 <body>
     @yield('content')
