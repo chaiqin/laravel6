f5abcf9b7d6ae2e4537b7d69db009930d47ef103
chq <17875511931@163.com>
2019-12-22 23:18:15 +0800

    '用户'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,8 +15,16 @@ class UserController extends Controller
         return view('user/index');
     }
 
-    public function getList(){
-        $data = User::paginate(1);
+    /**
+     * 获取用户列表
+     */
+    public function getList(Request $request){
+        $input = $request->all();
+        $where = [];
+        if(!empty($input['name'])){
+            $where['name'] = $input['name'];
+        }
+        $data = User::where($where)->paginate($input['limit']);
         return AjaxResult::pageReturn($data->total(),$data->getCollection()->toArray());
     }
 }
